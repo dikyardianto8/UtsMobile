c14b683107b43a16648fe0421e17df1982cc40b9
dikyardianto8 <Dickyardianto09@gmail.om>
2017-11-22 12:56:55 +0700

    commit

---
+++ mobile/src/main/java/com/example/dikyardianto/utsmobile/HasilSegitiga.java
@@ -0,0 +1,13 @@
+package com.example.dikyardianto.utsmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HasilSegitiga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hasil_segitiga);
+    }
+}
---
+++ mobile/src/main/java/com/example/dikyardianto/utsmobile/MainHasil.java
@@ -0,0 +1,13 @@
+package com.example.dikyardianto.utsmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHasil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_hasil);
+    }
+}
